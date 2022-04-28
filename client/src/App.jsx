b07a3bfebef6417e458df19ad6af87cba2063423
ryu57sp <ryuki.baske5@gmail.com>
2022-04-28 12:17:14 +0900

    [Update]HelloReactの表示

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { HelloReact } from './components/HelloReact'
+
+export const App = () => {
+  return(
+    <HelloReact/>
+  )
+}
--- client/src/components/HelloReact.jsx
+++ client/src/components/HelloReact.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default class HelloReact extends React.Component {
-    render() {
-      return (
-        <div classNam = "hello-react">
-          <h1>Hello React</h1>
-        </div>
-      )
-    }
+export const HelloReact = () => {
+  return(
+    <div className = "hello-react">
+      <h1>Hello React</h1>
+    </div>
+  )
 }
