3cee5567581da20055b7dbabfe27fbfd19d050b5
ryu57sp <ryuki.baske5@gmail.com>
2022-04-28 10:25:36 +0900

    HelloReactの表示

---
+++ client/src/components/HelloReact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HelloReact extends React.Component {
+    render() {
+      return (
+        <div classNam = "hello-react">
+          <h1>Hello React</h1>
+        </div>
+      )
+    }
+}
